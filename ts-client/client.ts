c70bddec592c34c205162575b333ef2ab88b89e2
Peter Olsen <olse0321@umn.edu>
2026-02-13 17:11:19 -0600

    add typescript client

---
+++ ts-client/client.ts
@@ -0,0 +1,32 @@
+// messed up method to ensure socket is connected before we send messages
+function ensureConnection(socket: WebSocket): Promise<void> {
+    return new Promise((resolve, reject) => {
+        socket.onopen = () => {
+            resolve();
+        };
+        
+        socket.onerror = () => {
+            reject();
+        }
+
+        socket.onclose = () => {
+            reject();
+        }
+    });
+}
+
+// "main" function
+(async () => {
+    // Establish initial connection
+    const socket = new WebSocket("ws://localhost:8080");
+    await ensureConnection(socket);
+
+    // Receive the first message then close
+    socket.onmessage = (ev: MessageEvent) => {
+        console.log("Got message from server:", ev.data);
+        socket.close()
+    }
+
+    // Send initial message once everything is ready
+    socket.send("Hello server from TypeScript!");
+})()

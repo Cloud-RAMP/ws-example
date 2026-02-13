feca643ee9e020d65ff9245ee030243aaf454928
Peter Olsen <olse0321@umn.edu>
2026-02-13 17:11:03 -0600

    finish client and server

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,5 +1,65 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"time"
 
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+)
+
+// Runs a simple client that connects to the web socket server and sends / receives messages
+func main() {
+	// Create a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Connect to ws server
+	conn, _, _, err := ws.Dialer{}.Dial(ctx, "ws://localhost:8080")
+	if err != nil {
+		fmt.Printf("Failed to connect to WebSocket server: %v\n", err)
+	}
+	defer conn.Close()
+
+	// Send a message to the server
+	message := []byte("Hello, WebSocket server!")
+	err = wsutil.WriteClientMessage(conn, ws.OpText, message)
+	if err != nil {
+		fmt.Printf("Failed to send message: %v\n", err)
+	}
+
+	// Read some amount of messages back from the server
+	// The nil parameter represents a pre-allocated buffer we can store the messages in.
+	// We could use this for performance, but it restricts the total message length
+	messages, err := wsutil.ReadServerMessage(conn, nil)
+	if err != nil {
+		fmt.Printf("Failed to recieve message: %v\n", err)
+	}
+
+	// Read messages that came back from the server
+	for _, msg := range messages {
+		msgString := string(msg.Payload)
+		fmt.Println("Response from server:", msgString)
+	}
+
+	// Example delay
+	time.Sleep(1 * time.Second)
+
+	// Repeat process again
+	message = []byte("Sending second message")
+	err = wsutil.WriteClientMessage(conn, ws.OpText, message)
+	if err != nil {
+		fmt.Printf("Failed to send message: %v\n", err)
+	}
+
+	messages, err = wsutil.ReadServerMessage(conn, nil)
+	if err != nil {
+		fmt.Printf("Failed to recieve message: %v\n", err)
+	}
+
+	for _, msg := range messages {
+		msgString := string(msg.Payload)
+		fmt.Println("Response from server:", msgString)
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,5 +1,70 @@
 package server
 
-func Start() {
+import (
+	"fmt"
+	"io"
+	"net/http"
 
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+)
+
+// Define start method here so that we can use it in testing
+func Start() {
+	http.HandleFunc("/", handleConnection)
+	http.ListenAndServe(":8080", nil)
+}
+
+// Connection handler function
+func handleConnection(w http.ResponseWriter, r *http.Request) {
+	// Updagrade the HTTP connection to WS
+	conn, _, _, err := ws.UpgradeHTTP(r, w)
+	if err != nil {
+		// handle error
+		fmt.Println("Error upgrading protocols")
+		return
+	}
+
+	// Here we can access info about the client, good for logging + billing purposes
+	fmt.Println("New client connected:")
+	fmt.Println("IP:", r.RemoteAddr)
+	fmt.Println("Domain:", r.Host)
+
+	// Start separate goroutine for each connection
+	// This does not scale super well, think about maybe worker pools or something
+	go func() {
+		defer conn.Close() // when the function returns, close the connection
+
+		// infinite loop
+		for {
+
+			// Read data from the client on the connection
+			// see https://datatracker.ietf.org/doc/html/rfc6455#section-5.5 for info on "op"
+			msg, op, err := wsutil.ReadClientData(conn)
+			if err == io.EOF {
+				fmt.Println("Client disconnected")
+				return
+			} else if err != nil {
+				// handle error
+				fmt.Println("error reading:", err)
+				return
+			}
+
+			// Probably not super necessary, good safeguard though
+			// Other operation types CAN be sent
+			if op.IsData() {
+				fmt.Println("Client message:", string(msg))
+			}
+
+			// Write a message to the client as the server (in this case, echo it)
+			err = wsutil.WriteServerMessage(conn, op, msg)
+			if err == io.EOF {
+				fmt.Println("Client disconnected")
+				return
+			} else if err != nil {
+				fmt.Println("error reading:", err)
+				return
+			}
+		}
+	}()
 }
